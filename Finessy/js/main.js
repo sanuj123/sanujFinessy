4c5e7bf4df6b6792a7754b1a432b21a62d52fcbf
sanuj123 <sanujs97@gmail.com>
2017-10-14 19:15:30 +0530

    changes

--- Finessy/js/main.js
+++ Finessy/js/main.js
@@ -1,3 +1,18 @@
+jQuery.easing['jswing'] = jQuery.easing['swing'];
+
+jQuery.extend( jQuery.easing,
+    {
+        def: 'easeOutQuad',
+        easeInOutExpo: function (x, t, b, c, d) {
+            if (t == 0) return b;
+            if (t == d) return b + c;
+            if ((t /= d / 2) < 1) return c / 2 * Math.pow(2, 10 * (t - 1)) + b;
+            return c / 2 * (-Math.pow(2, -10 * --t) + 2) + b;
+        }
+    });
+
+
+
 var contentWayPoint = function() {
     var i = 0;
     $('.animate-box').waypoint( function( direction ) {
@@ -25,7 +40,7 @@ var contentWayPoint = function() {
 };
 
 $(document).ready(function(){
-  contentWayPoint();
+    contentWayPoint();
 
 
 });
