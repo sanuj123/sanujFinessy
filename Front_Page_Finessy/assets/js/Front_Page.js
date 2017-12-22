f5d4f6e9fe3dbbc09bd6eeebf7486494ce913c74
sanuj123 <sanujs97@gmail.com>
2017-12-22 15:30:55 +0530

    Add files via upload

---
+++ Front_Page_Finessy/assets/js/Front_Page.js
@@ -0,0 +1,51 @@
+jQuery.easing['jswing'] = jQuery.easing['swing'];
+
+jQuery.extend( jQuery.easing,
+    {
+        def: 'easeOutQuad',
+        easeInOutExpo: function (x, t, b, c, d) {
+            if (t == 0) return b;
+            if (t == d) return b + c;
+            if ((t /= d / 2) < 1) return c / 2 * Math.pow(2, 10 * (t - 1)) + b;
+            return c / 2 * (-Math.pow(2, -10 * --t) + 2) + b;
+        }
+    });
+
+$(function(){
+    $('.navbar').affix({
+        offset: {
+            /* Affix the navbar after scroll below header */
+            top: $("header").outerHeight(true)}
+    });
+});
+var contentWayPoint = function() {
+    var i = 0;
+    $('.animate-box').waypoint( function( direction ) {
+
+        if( direction === 'down' && !$(this.element).hasClass('animated') ) {
+
+            i++;
+
+            $(this.element).addClass('item-animate');
+            setTimeout(function(){
+
+                $('body .animate-box.item-animate').each(function(k){
+                    var el = $(this);
+                    setTimeout( function () {
+                        el.addClass('fadeInUp animated');
+                        el.removeClass('item-animate');
+                    },  k * 200, 'easeInOutExpo' );
+                });
+
+            }, 100);
+
+        }
+
+    } , { offset: '85%' } );
+};
+
+$(document).ready(function(){
+    contentWayPoint();
+
+
+});
